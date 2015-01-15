da307c6f37d84e3cea16935197701b985b76de30
Denis Gamza <diesel_ru@mail.ru>
2015-01-15 17:32:13 +0800

    main

---
+++ app/src/main/java/ru/dsoft38/smssender/sms_sender/FileDialog.java
@@ -0,0 +1,189 @@
+package ru.dsoft38.smssender.sms_sender;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Environment;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+
+/**
+ * Created by user on 15.01.2015.
+ */
+public class FileDialog extends AlertDialog{
+    Context context;
+    File dir;
+    String[] files;
+    AlertDialog.Builder builder;
+    AlertDialog dialog;
+    FilenameFilter filenameFilter;
+    PluginProperties plugin_props;
+    FileDialogDepends fileDialogDepends;
+
+    public static String FILENAME_FILTER =
+            "(?i)^feeder\\-.*?\\.html$";
+    public static String CURRENT_PATH = "/sdcard";
+
+    public static String ROOT_PATH =
+            Environment.getExternalStorageDirectory().getPath();;
+
+
+    public FileDialog(Context context) {
+        super(context);
+        this.context=context;
+
+        filenameFilter=new FilenameFilter(){
+            @Override
+            public boolean accept(File directory, String fileName) {
+                return fileName.matches(FILENAME_FILTER);
+            }
+        };
+    }
+
+    /**
+     * Вызов диалога открытия файла.
+     * @param fileDialogDepends – объект, ссылка
+     * на который передается из вызывающей activity для
+     * взаимосвязи с ней.
+     */
+    public void openFileDialog(FileDialogDepends fileDialogDepends){
+        this.fileDialogDepends=fileDialogDepends;
+        openFileDialog();
+    }
+
+    /**
+     * Внутренняя процедура для рекурсивного
+     * переоткрытия диалога файла.
+     */
+    private void openFileDialog(){
+        builder = new AlertDialog.Builder(context);
+
+        dir=new File(CURRENT_PATH);
+        builder.setTitle(R.string.select_file);
+        files=concatAll(dirs(dir),files(dir,filenameFilter));
+        builder.setItems(files, listenerFileDialog);
+        builder.setNegativeButton(android.R.string.cancel, null);
+
+        dialog = builder.create();
+        dialog.show();
+    }
+
+    /**
+     * Определяется реакция на нажатие кнопки на диалоге:
+     * – если выбран <..> – переходим
+     *    на уровень вверх и вызываем openFileDialog
+     * – если выбрана папка – переходим
+     *    в папку и вызываем openFileDialog
+     * – если выбран файл – сохраняем текущий
+     *    путь в настройки, вызываем fileSelected,
+     * – если нажата отмена – закрываем диалог
+     */
+    private DialogInterface.OnClickListener listenerFileDialog=
+            new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+
+                    if (files[which].equals("..")){//НАЖАТО НА <..>
+                        dir=new File(dir.getParent());
+                    }else dir=new File(CURRENT_PATH+"/"+files[which]);
+                    if(dir.isFile()){ //ВЫБРАН ФАЙЛ
+                        SharedPreferences.Editor ed = context.getSharedPreferences(
+                                "preferences", Context.MODE_PRIVATE).edit();
+                        ed.putString("pref_current_path", CURRENT_PATH);
+                        ed.apply();
+                        fileSelected(dir);
+                    }else if(dir.isDirectory()){ //ВЫБРАНА ПАПКА
+                        if (!files[which].equals(".."))
+                            dir=new File(CURRENT_PATH+"/"+files[which]);
+                        CURRENT_PATH=dir.toString();
+                        openFileDialog(fileDialogDepends);
+                    }
+                }
+            };
+
+    /**
+     * Файл выбран.
+     * @param file – выбранный файл.
+     */
+    public void fileSelected(File file){
+
+        //ЗДЕСЬ ПИШЕМ ПРОЦЕДУРЫ, КОТОРЫЕ РЕАЛИЗУЮТСЯ, КОГДА
+        //ФАЙЛ ВЫБРАН. ТО ЕСТЬ ОТКРЫТИЕ ФАЙЛА, ОБРАБОТКА,
+        //КОПИРОВАНИЕ И ТАК ДАЛЕЕ.
+
+        //Здесь проводим взаимосвязь с вызывающей activity.
+        fileDialogDepends.refresh();
+
+    }
+
+    /**
+     * Выдает отсортированный список папок из папки path.
+     * 1) Игнорируются папки, начинающиеся с точки.
+     * 2) В начало списка помещается пункт "..".
+     * 3) Сортировка осуществляется в предпоследней строке, для
+     * регистронезависимого варианта необходимо определить
+     * соответствующий компаратор.
+     */
+    public static String[] dirs(File path){
+        final List<String> files = new ArrayList<String>();
+
+        if(!path.toString().equals(ROOT_PATH))files.add("..");
+
+        for(File a:path.listFiles()){
+            if(a.isDirectory() && !a.getName().
+                    toString().startsWith("."))
+                files.add(a.getName().toString()+"/");
+        }
+        String[] res=(String[])
+                files.toArray(new String[files.size()]);
+        Arrays.sort(res, new SortedByName());
+        return res;
+    }
+
+    /**
+     * Выдает отсортированный список файлов из папки path,
+     * отфильтрованный в соответствии с фильтром filter.
+     * Сортировка осуществляется в предпоследней строке, для
+     * регистронезависимого варианта необходимо определить
+     * соответствующий компаратор.
+     */
+    public static String[] files(File path,
+                                 FilenameFilter filter){
+        final List<String> files = new ArrayList<String>();
+
+        for(File a:path.listFiles(filter))
+            if(a.isFile())files.add(a.getName().toString());
+
+        String[] res=(String[])
+                files.toArray(new String[files.size()]);
+        Arrays.sort(res, new SortedByName());
+        return res;
+    }
+
+    /**
+     * Объединение массивов.
+     * @param first
+     * @param rest
+     * @return
+     */
+    public static <T> T[] concatAll(T[] first, T[]... rest) {
+        int totalLength = 0;
+        if(first!=null)totalLength=first.length;
+        for (T[] array : rest)
+            if(array!=null) totalLength += array.length;
+        T[] result = Arrays.copyOf(first, totalLength);
+        int offset = first.length;
+        for (T[] array : rest) {
+            if(array!=null){
+                System.arraycopy(array, 0,
+                        result, offset, array.length);
+                offset += array.length;
+            }
+        }
+        return result;
+    }
+}
---
+++ app/src/main/java/ru/dsoft38/smssender/sms_sender/MainActivity.java
@@ -0,0 +1,252 @@
+package ru.dsoft38.smssender.sms_sender;
+
+import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+
+public class FileDialogDepends{
+    public FileDialogDepends refresh(){
+        refresh();
+        return this;
+    }
+}
+
+public class MainActivity extends ActionBarActivity {
+
+    ImageButton btnBrowse;
+    ImageButton btnStart;
+    ImageButton btnPause;
+    ImageButton btnStop;
+    ImageButton btnClear;
+
+    TextView tvPhoneNumberListFilePatch;
+    EditText editMessageTest;
+
+    ProgressBar progressBar;
+
+    int CurrentIndexSMS = 0;
+    boolean isStop = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnBrowse = (ImageButton) findViewById(R.id.imgButtonBrowse);
+        btnStart = (ImageButton) findViewById(R.id.imgButtonSend);
+        btnPause = (ImageButton) findViewById(R.id.imgButtonPause);
+        btnStop = (ImageButton) findViewById(R.id.imgButtonStop);
+
+        editMessageTest = (EditText) findViewById(R.id.editMessageText);
+        tvPhoneNumberListFilePatch = (TextView) findViewById(R.id.tvPhoneNumberListPath);
+
+        // Назначаем обработчик нажатия на кнопку Отправить СМС
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // Делаем кнопку не активной
+                btnBrowse.setEnabled(false);
+                btnStart.setEnabled(false);
+
+                // Выбран файл с номерами телефонов
+                if(tvPhoneNumberListFilePatch.getText().length() == 0) {
+                    Toast.makeText(getApplicationContext(),
+                            "Выберите файл со списком номеров для отправки!",
+                            Toast.LENGTH_LONG).show();
+                    return;
+                } else {
+                    List<String> strNumbers = readFile("");
+                    String message = editMessageTest.getText().toString();
+
+                    int maxCount = strNumbers.size();
+
+                    if (maxCount > 100)
+                        maxCount = 100;
+
+                    progressBar.setMax(maxCount);
+
+                    for (int i = CurrentIndexSMS; i < maxCount; i++) {
+                        // Если нажали остановку или паузу отправки
+                        if(isStop)
+                        {
+                            Toast.makeText(getApplicationContext(),
+                                    "Отправка остановлена",
+                                    Toast.LENGTH_LONG).show();
+                            return;
+                        }
+
+                        // Удаляем не нужные символы
+                        String num = strNumbers.get(i).replace("-", "").replace(";", "").replace(" ", "").trim() ;
+
+                        // Проверяем длину номера 11 символов или 12, если с +
+                        if(num.length() == 11 || (num.substring(1, 1) == "+" && num.length() == 12)) {
+                            sendSMSMessage(num, message);
+                        }
+
+                        CurrentIndexSMS = i + 1;
+                        progressBar.setProgress(CurrentIndexSMS);
+                    }
+                }
+            }
+        });
+
+        // Назначаем обработчик нажатия на кнопку выбора файла
+        btnBrowse.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+                btnStart.setEnabled(true);
+                btnPause.setEnabled(true);
+                btnStop.setEnabled(true);
+            }
+        });
+
+        // Назначаем обработчик нажатия на кнопку приостановки отправки
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // Останавливаем отправку
+                isStop = true;
+
+                btnStart.setEnabled(true);
+                btnPause.setEnabled(false);
+                btnStop.setEnabled(true);
+            }
+        });
+
+        // Назначаем обработчик нажатия на кнопку остановки отправки
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // Останавливаем отправку
+                isStop = true;
+                CurrentIndexSMS = 0;
+
+                btnStart.setEnabled(true);
+                btnPause.setEnabled(true);
+                btnStop.setEnabled(false);
+
+                btnBrowse.setEnabled(true);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Отправка СМС
+    protected void sendSMSMessage(String phoneNo, String message) {
+        Log.i("Send SMS", "Phone: " + phoneNo);
+
+        //String phoneNo = txtphoneNo.getText().toString();
+        //String message = txtMessage.getText().toString();
+
+        try {
+            //SmsManager smsManager = SmsManager.getDefault();
+            //smsManager.sendTextMessage(phoneNo, null, message, null, null);
+            Toast.makeText(getApplicationContext(), "SMS sent.",
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(),
+                    "SMS faild, please try again.",
+                    Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+
+    // Чтение файла с номерами
+    List<String> readFile(String filePath){
+
+        List<String> strNumbers = null;
+        File sdcard = Environment.getExternalStorageDirectory();
+
+        //Создаём объект файла
+        File file = new File(sdcard, filePath);
+
+        //Read text from file
+        //StringBuilder text = new StringBuilder();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                //text.append(line);
+                //text.append('\n');
+                strNumbers.add(line);
+            }
+        }
+        catch (IOException e) {
+            //Ошибка
+        }
+
+        //Log.d("Data", text);
+
+        return strNumbers;
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        // TODO Auto-generated method stub
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        // TODO Auto-generated method stub
+        super.onStop();
+    }
+}
